08a12c9865d909674bc7976857fc3ee22047cb44
yacth_Mon <yacthmon@gmail.com>
2018-04-23 19:13:55 +0700

    Add addBackTranscript

--- model/sheet_manage.js
+++ model/sheet_manage.js
@@ -17,7 +17,7 @@ class sheet_manage {
     return new Promise((resolve, reject) => {
       doc.useServiceAccountAuth(this.creds, () => {
         doc.getInfo((err, info) => {
-          if(err) return reject(err);
+          if (err) return reject(err);
           console.log(`Loaded doc: ${info.title} by ${info.author.email}`);
           for (let sheet of info.worksheets) {
             console.log(`sheet : ${sheet.title} ${sheet.rowCount} x ${sheet.colCount}`);
@@ -42,21 +42,62 @@ class sheet_manage {
         if (err) return reject(err);
         for (let i in cells) {
           if (cells[i].value == '') {
+            i = parseInt(i);
             cells[i].value = menu;
-            cells[parseInt(i)+1].numericValue = amount;
+            cells[i + 1].numericValue = amount;
             this.front_shop.bulkUpdateCells(cells);
-            console.log(`[${i},${parseInt(i)+1}] Add transcript ${menu} with ${amount} ea.`);
-            return resolve();
+            console.log(`[${i},${i + 1}] Add front transcript ${menu} with ${amount} ea.`);
+            return resolve((i / 2) + 1);
           }
         }
         return resolve(false);
       })
     })
   }
+
+  addBackTranscript(menu, { customer_name, location, phone }) {
+    return new Promise((resolve, reject) => {
+      switch (undefined) {
+        case customer_name:
+          reject("Must provide customer_name");
+          break;
+        case location:
+          reject("Must provide location");
+          break;
+        case phone:
+          reject("Must provide phone");
+          break;
+      }
+      this.back_shop.getCells({
+        'min-row': 3,
+        'max-row': 52,
+        'min-col': 2,
+        'max-col': 7,
+        'return-empty': true
+      }, (err, cells) => {
+        if (err) return reject(err);
+        for (let i in cells) {
+          if (cells[i].value == '') {
+            i = parseInt(i);
+            cells[i].value = menu;
+            cells[i + 1].numericValue = 30;
+            cells[i + 2].value = customer_name;
+            cells[i + 3].value = location;
+            cells[i + 4].value = phone;
+            cells[i + 5].value = "รับออเดอร์";
+            this.back_shop.bulkUpdateCells(cells);
+            console.log(`[${i},${i + 1}] Add back transcript ${menu} with 1 ea.`);
+            return resolve((i / 6) + 1);
+          }
+        }
+      })
+    })
+  }
 }
 
 let sheet = new sheet_manage();
-sheet.connect().then(()=>{
-  sheet.addFrontTranscript("ทดสอบจาก API", 2)
+sheet.connect().then(() => {
+  // sheet.addFrontTranscript("ทดสอบจาก API", 2).then(id=>console.log("Add at id : " + id))
+  // sheet.addBackTranscript("ทดสอบ order จาก api", { customer_name: "ยอสเอง", location: "หน้าตึก SIT", phone: "08ไม่บอกหรอก" }).then(id => console.log("Add at id : " + id));
 })
 module.exports = sheet_manage;
