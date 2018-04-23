be299c1168d4394b469903034cfbaa3ea3a1d46f
yacth_Mon <yacthmon@gmail.com>
2018-04-23 18:53:06 +0700

    Remove exmaple and add sheet_manage class

--- model/sheet_manage.js
+++ model/sheet_manage.js
@@ -6,84 +6,57 @@ let doc = new GoogleSpreadsheet('1UA5H3zmHr8QF_LBLd2qlZg5lRs41Ha1Xl9eXgFAWjHA');
 let front_shop;
 let back_shop;
 
-async.series([
-  function setAuth(step) {
-    // see notes below for authentication instructions!
-    let creds = require('../google-generated-creds.json');
-    doc.useServiceAccountAuth(creds, step);
-  },
-  function getInfoAndWorksheets(step) {
-    doc.getInfo(function (err, info) {
-      console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
-      for (let sheet of info.worksheets) {
-        console.log(`sheet : ${sheet.title} ${sheet.rowCount} x ${sheet.colCount}`);
-      }
-      front_shop = info.worksheets[0];
-      back_shop = info.worksheets[1];
-      step();
-    });
-  },
-  // function workingWithRows(step) {
-  //   // google provides some query options
-  //   sheet.getRows({
-  //     offset: 1,
-  //     limit: 20,
-  //     orderby: 'col2'
-  //   }, function( err, rows ){
-  //     console.log('Read '+rows.length+' rows');
-  //     console.log(rows);
-
-  //     // the row is an object with keys set by the column headers
-  //     rows[0].colname = 'new val';
-  //     rows[0].save(); // this is async
-
-  //     // deleting a row
-  //     rows[0].del();  // this is async
-
-  //     step();
-  //   });
-  // },
-  function workingWithCells(step) {
-    front_shop.getCells({
-      'min-row': 3,
-      'max-row': 52,
-      'min-col': 2,
-      'max-col': 3,
-      'return-empty': true
-    }, function (err, cells) {
-      // let cell = cells[0];
-      // console.log('Cell R'+cell.row+'C'+cell.col+' = '+cell.value);
-
-      // // cells have a value, numericValue, and formula
-      // cell.value == '1'
-      // cell.numericValue == 1;
-      // cell.formula == '=ROW()';
-
-      // // updating `value` is "smart" and generally handles things for you
-      // cell.value = 123;
-      // cell.value = '=A1+B2'
-      // cell.save(); //async
-
-      // bulk updates make it easy to update many cells at once
-      // cells[0].value = 1;
-      // cells[1].value = 2;
-      // cells[2].formula = '=A1+B1';
-      // sheet.bulkUpdateCells(cells); //async
-
-      console.log(cells.length);
-      cells[0].value = "ไก่สูตรดับเบิ้ลสมุนไพร";
-      cells[1].numericValue = 2;
-      
-      // let i = 1
-      // for (let cell of cells) {
-      //   cell.value = i++;
-      // }
-      front_shop.bulkUpdateCells(cells); //async
-      step();
-    });
+class sheet_manage {
+  constructor() {
+    this.creds = require('../google-generated-creds.json');
+    this.front_shop;
+    this.back_shop;
   }
-], function (err) {
-  if (err) {
-    console.log('Error: ' + err);
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      doc.useServiceAccountAuth(this.creds, () => {
+        doc.getInfo((err, info) => {
+          if(err) return reject(err);
+          console.log(`Loaded doc: ${info.title} by ${info.author.email}`);
+          for (let sheet of info.worksheets) {
+            console.log(`sheet : ${sheet.title} ${sheet.rowCount} x ${sheet.colCount}`);
+          }
+          this.front_shop = info.worksheets[0];
+          this.back_shop = info.worksheets[1];
+          resolve();
+        });
+      });
+    })
   }
-});
+
+  addFrontTranscript(menu, amount) {
+    return new Promise((resolve, reject) => {
+      this.front_shop.getCells({
+        'min-row': 3,
+        'max-row': 52,
+        'min-col': 2,
+        'max-col': 3,
+        'return-empty': true
+      }, (err, cells) => {
+        if (err) return reject(err);
+        for (let i in cells) {
+          if (cells[i].value == '') {
+            cells[i].value = menu;
+            cells[parseInt(i)+1].numericValue = amount;
+            this.front_shop.bulkUpdateCells(cells);
+            console.log(`[${i},${parseInt(i)+1}] Add transcript ${menu} with ${amount} ea.`);
+            return resolve();
+          }
+        }
+        return resolve(false);
+      })
+    })
+  }
+}
+
+let sheet = new sheet_manage();
+sheet.connect().then(()=>{
+  sheet.addFrontTranscript("ทดสอบจาก API", 2)
+})
+module.exports = sheet_manage;
